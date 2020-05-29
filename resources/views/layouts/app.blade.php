241a7f0673bccc30c5956f7e088703de1f051c8c
pav <phrynchenko@ukr.net>
2020-05-29 22:06:23 +0300

    29.05.20 UPD:Creating new pages

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>@yield('title-block')</title>
+</head>
+<body>
+  @yield('content')
+
+  @include('include.aside')
+
+
+</body>
+</html>
